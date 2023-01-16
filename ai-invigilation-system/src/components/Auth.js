0c48f558a439df26f91bb4e4922cb82c8acf5b07
yukizyx <yukizyx230@gmail.com>
2023-01-16 15:58:05 -0500

    Implement main page

---
+++ ai-invigilation-system/src/components/Auth.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Auth.css";
+import { Link } from "react-router-dom";
+
+export function MainPage() {
+  return (
+    <div className="parent">
+      <div className="header">AI Invigilator System</div>
+
+      <div className="body">
+        <div>User Authentication</div>
+        <div>
+          <button className="btn-usertype">Supervisor</button>
+        </div>
+        <div>
+          <button className="btn-usertype">Staff</button>
+        </div>
+      </div>
+    </div>
+  );
+}
