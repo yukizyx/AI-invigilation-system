d4df4925c854303cbf8da7781bda589e4b702005
Xing Li <59614094+IGTheShyQwQ@users.noreply.github.com>
2023-01-24 22:28:59 -0500

    video controller class created

---
+++ AI Invigilation/Exam Manager/video_controller.py
@@ -0,0 +1,17 @@
+class video_controller:
+
+
+    def __init__(self):
+        pass
+
+    def start_Recording(self, date):
+        pass
+
+
+    def end_Recording(self):
+        pass
+
+
+    def send_Next_detection_frame(self):
+        pass
+
