bd364d66bd7c12077e8870b678b85d75f1c2d9c9
yukizyx <yukizyx230@gmail.com>
2023-02-25 11:43:07 -0500

    Modify some code in user login

--- AI_Invigilation/Auth.py
+++ AI_Invigilation/Auth.py
@@ -1,11 +1,14 @@
 from flask import Flask, redirect, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/auth-supervisor',methods = ['POST', 'GET'])
 def sup_login():
     if request.method == 'POST':
         user_id = request.form['id']
-        code = request.form['code']
+        code = request.form['code']  
+        pass_supervisor = hash(user_id)
+        print(pass_supervisor)
         
         if (user_id in ["sup1", "sup2", "sup3"]) and (code == "11223344"):
             return redirect('http://localhost:3000/supervisor-home')
@@ -18,6 +21,8 @@ def stf_login():
     if request.method == 'POST':
         user_id = request.form['id']
         code = request.form['code']
+        pass_staff = hash(user_id)
+        print(pass_staff)
         
         if (user_id in ["stf1", "stf2", "stf3"]) and (code == "11223344"):
             return redirect('http://localhost:3000/staff-home')
@@ -25,4 +30,4 @@ def stf_login():
             return redirect('http://localhost:3000/auth-staff')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True) 
